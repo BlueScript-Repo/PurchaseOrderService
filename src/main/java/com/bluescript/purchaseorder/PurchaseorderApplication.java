33588754161888b0cd4cd4df1d3f0e84e2091940
BlueScript-Repo <repo@bluescripts.in>
2020-12-23 20:05:18 +0530

    commit for PurchaseOrder

---
+++ src/main/java/com/bluescript/purchaseorder/PurchaseorderApplication.java
@@ -0,0 +1,13 @@
+package com.bluescript.purchaseorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseorderApplication.class, args);
+	}
+
+}
